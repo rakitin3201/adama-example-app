03d356ecc1190d8fd69fb28c3ca43d17a01a658b
Adama NIADA <a77272199@gmail.com>
2023-03-02 09:41:57 +0000

    Quatrieme commit

--- shopping/urls.py
+++ shopping/urls.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 from django.urls import path
-from .views import index, afficher_produit
+from shopping.views import index, afficher_produit
 
 urlpatterns = [
-    path('', index, name="e_commerce-index"),
+    path('', index, name="shopping-index"),
     path('produit-<str:id_produit>/', afficher_produit, name="afficher-produit"),
 ]
