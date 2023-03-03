2e7d676e78dfd0a07ea95151051b40884cb15b83
Adama NIADA <a77272199@gmail.com>
2023-03-03 08:54:11 +0000

    Update buil.sh files

--- vercel_app/settings.py
+++ vercel_app/settings.py
@@ -84,6 +84,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        # 'USER': '',
+        # 'PASSWORD': '',
+        # 'HOST': '',
+        # 'PORT': '',  
     }
 }
 
