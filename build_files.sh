475c4095e5143ed6fe96cbdb2d039ed32f1dbdb3
Adama NIADA <a77272199@gmail.com>
2023-03-03 09:14:14 +0000

    Commit 3

--- build_files.sh
+++ build_files.sh
@@ -1,10 +1,13 @@
 # build the project
 # pip install -r requirements.txt
+echo "build the project"
 pip -m pip install -r requirements.txt
 
 # Make migration
+echo "Make migration"
 python3.9 manage.py makemigrations --noinput
 python3.9 manage.py migrate --noinput
 
 # Collect static
+echo "Collect static"
 python3.9 manage.py collectstatic --noinput --clear
