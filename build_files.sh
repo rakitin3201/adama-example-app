c27a2d8a08cc4b1ca670802d9d6eaf811d247d53
Adama NIADA <a77272199@gmail.com>
2023-03-03 08:14:33 +0000

    Adding database commit

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,10 @@
-pip install -r requirements.txt
-python3.9 manage.py collectstatic
+# build the project
+# pip install -r requirements.txt
+pip -m pip install -r requirements.txt
+
+# Make migration
+python3.9 manage.py makemigrations --noinput
+python3.9 manage.py migrate --noinput
+
+# Collect static
+python3.9 manage.py collectstatic --noinput --clear
--- vercel_app/settings.py
+++ vercel_app/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -79,11 +81,13 @@ WSGI_APPLICATION = 'vercel_app.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
+DATABASES['default'] = dj_database_url.config()
 
 
 # Password validation
